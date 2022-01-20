bd9613164059c0201f41e4c9869fc2ffab018fa9
Ruben Mertens <ruben.mertens@infosupport.com>
2022-01-20 15:28:08 +0100

    change imports to build client

--- reactive-music-client/src/main.ts
+++ reactive-music-client/src/main.ts
@@ -1,125 +1,115 @@
-import { Message } from './../../server/src/main'
 import {
-  connect,
-  connectable,
   filter,
-  from,
   fromEvent,
   interval,
   map,
-  Observable,
   share,
   Subject,
-  subscribeOn,
   switchMap,
-  takeUntil,
-  tap,
-} from 'rxjs'
-import './style.css'
+  takeUntil
+} from "rxjs";
+import "../assets/dominik-scythe-412561-cropped.jpg";
+import "../assets/PressStart2P-Regular.ttf";
+import { configuration } from "./configuration";
+import { PianoService } from "./services/PianoService";
+import "./style.css";
 
-import { UserManager } from './services/UserManager'
-import { configuration } from './configuration'
-import { PianoService } from './services/PianoService'
-import { EventEmitter } from 'stream'
-import { parse } from 'path/posix'
-
-import classes from './style.css'
-
-const piano = new PianoService()
-const userManager = new UserManager()
+const piano = new PianoService();
 
 export type WebSocketConnection = {
-  wss: WebSocket
-  status: 'connected' | 'reconnected' | 'disconnected'
-  reconnectIn: number
-}
+  wss: WebSocket;
+  status: "connected" | "reconnected" | "disconnected";
+  reconnectIn: number;
+};
 
 window.onload = () => {
-  console.log('loaded')
-  piano.preLoadAudio()
+  console.log("loaded");
+  piano.preLoadAudio();
 
-  const loadButton = document.getElementById('join-btn') as HTMLButtonElement
-  const connectedAs = document.getElementById('connected-as') as HTMLDivElement
+  const loadButton = document.getElementById("join-btn") as HTMLButtonElement;
+  const connectedAs = document.getElementById("connected-as") as HTMLDivElement;
 
-  let wss: WebSocket
-  fromEvent<MouseEvent>(loadButton, 'click').subscribe((e) => {
-    console.log('join clicked')
-    wss = new WebSocket(configuration.websocketUrl)
-    setupWebSocket()
-    loadButton.disabled = true
-  })
+  let wss: WebSocket;
+  fromEvent<MouseEvent>(loadButton, "click").subscribe((e) => {
+    console.log("join clicked");
+    wss = new WebSocket(configuration.websocketUrl);
+    setupWebSocket();
+    loadButton.disabled = true;
+  });
 
-  const recconected$ = new Subject<void>()
+  const recconected$ = new Subject<void>();
   const setupWebSocket = () => {
-    fromEvent(wss, 'close').subscribe((e) => {
-      connectedAs.innerText = 'Connection closed'
-      loadButton.disabled = false
-    })
-    fromEvent(wss, 'close')
+    fromEvent(wss, "close").subscribe((e) => {
+      connectedAs.innerText = "Connection closed";
+      loadButton.disabled = false;
+    });
+    fromEvent(wss, "close")
       .pipe(switchMap((close) => interval(1000).pipe(takeUntil(recconected$))))
       .subscribe((e) => {
-        recconected$.next()
-        wss = new WebSocket(configuration.websocketUrl)
-        setupWebSocket()
-      })
+        recconected$.next();
+        wss = new WebSocket(configuration.websocketUrl);
+        setupWebSocket();
+      });
 
-    fromEvent(wss, 'open').subscribe((e) => {
-      console.log('opened connection', e)
-      recconected$.next()
-    })
+    fromEvent(wss, "open").subscribe((e) => {
+      console.log("opened connection", e);
+      recconected$.next();
+    });
 
-    const parsedMessage$ = fromEvent<MessageEvent>(wss, 'message').pipe(
+    const parsedMessage$ = fromEvent<MessageEvent>(wss, "message").pipe(
       map((e) => JSON.parse(e.data)),
       share()
-    )
+    );
 
     parsedMessage$
       .pipe(
-        filter((e) => e.type === 'connected-as'),
+        filter((e) => e.type === "connected-as"),
         map((e) => e.data as { id: string })
       )
       .subscribe((e) => {
-        connectedAs.innerText = `Connected as ${e.id}`
-      })
+        connectedAs.innerText = `Connected as ${e.id}`;
+      });
 
     parsedMessage$
       .pipe(
-        filter((e) => e.type === 'play-note'),
-        map((e) => e.data as { note: string; velocity: number; duration: number })
+        filter((e) => e.type === "play-note"),
+        map(
+          (e) => e.data as { note: string; velocity: number; duration: number }
+        )
       )
       .subscribe((e) => {
-        console.log('playing', e)
-        piano.play(e)
-      })
+        console.log("playing", e);
+        piano.play(e);
+      });
 
     parsedMessage$
       .pipe(
-        filter((e) => e.type === 'ping'),
+        filter((e) => e.type === "ping"),
         map((e) => e.data as { time: number; i: number })
       )
       .subscribe((pingmsg) => {
         wss.send(
           JSON.stringify({
-            type: 'pong',
+            type: "pong",
             data: {
               ping: Date.now() - pingmsg.time,
               i: pingmsg.i,
             },
           })
-        )
-      })
-  }
-}
+        );
+      });
+  };
+};
 
 const makePianoDom = () => {
-  const container = document.getElementById('piano')
+  const container = document.getElementById("piano");
   for (const key in piano.piano) {
     if (Object.prototype.hasOwnProperty.call(piano.piano, key)) {
-      const note = piano.piano[key]
-      const btn = document.createElement('button')
-      btn.innerText = key
-      btn.onclick = () => note.play()
-      container?.appendChild(btn)
+      const note = piano.piano[key];
+      const btn = document.createElement("button");
+      btn.innerText = key;
+      btn.onclick = () => note.play();
+      container?.appendChild(btn);
     }
   }
-}
+};
--- reactive-music-client/src/services/PianoService.ts
+++ reactive-music-client/src/services/PianoService.ts
@@ -1,12 +1,76 @@
-import { Howl, Howler } from 'howler'
+import { Howl, Howler } from "howler";
+import A1 from "../../assets/audio_files/piano/A1.webm";
+import A2 from "../../assets/audio_files/piano/A2.webm";
+import A3 from "../../assets/audio_files/piano/A3.webm";
+import A4 from "../../assets/audio_files/piano/A4.webm";
+import A5 from "../../assets/audio_files/piano/A5.webm";
+import Ab1 from "../../assets/audio_files/piano/Ab1.webm";
+import Ab2 from "../../assets/audio_files/piano/Ab2.webm";
+import Ab3 from "../../assets/audio_files/piano/Ab3.webm";
+import Ab4 from "../../assets/audio_files/piano/Ab4.webm";
+import Ab5 from "../../assets/audio_files/piano/Ab5.webm";
+import B1 from "../../assets/audio_files/piano/B1.webm";
+import B2 from "../../assets/audio_files/piano/B2.webm";
+import B3 from "../../assets/audio_files/piano/B3.webm";
+import B4 from "../../assets/audio_files/piano/B4.webm";
+import B5 from "../../assets/audio_files/piano/B5.webm";
+import B6 from "../../assets/audio_files/piano/B6.webm";
+import Bb1 from "../../assets/audio_files/piano/Bb1.webm";
+import Bb2 from "../../assets/audio_files/piano/Bb2.webm";
+import Bb3 from "../../assets/audio_files/piano/Bb3.webm";
+import Bb4 from "../../assets/audio_files/piano/Bb4.webm";
+import Bb5 from "../../assets/audio_files/piano/Bb5.webm";
+import Bb6 from "../../assets/audio_files/piano/Bb6.webm";
+import C1 from "../../assets/audio_files/piano/C1.webm";
+import C2 from "../../assets/audio_files/piano/C2.webm";
+import C3 from "../../assets/audio_files/piano/C3.webm";
+import C4 from "../../assets/audio_files/piano/C4.webm";
+import C5 from "../../assets/audio_files/piano/C5.webm";
+import C6 from "../../assets/audio_files/piano/C6.webm";
+import D1 from "../../assets/audio_files/piano/D1.webm";
+import D2 from "../../assets/audio_files/piano/D2.webm";
+import D3 from "../../assets/audio_files/piano/D3.webm";
+import D4 from "../../assets/audio_files/piano/D4.webm";
+import D5 from "../../assets/audio_files/piano/D5.webm";
+import Db1 from "../../assets/audio_files/piano/Db1.webm";
+import Db2 from "../../assets/audio_files/piano/Db2.webm";
+import Db3 from "../../assets/audio_files/piano/Db3.webm";
+import Db4 from "../../assets/audio_files/piano/Db4.webm";
+import Db5 from "../../assets/audio_files/piano/Db5.webm";
+import Db6 from "../../assets/audio_files/piano/Db6.webm";
+import E1 from "../../assets/audio_files/piano/E1.webm";
+import E2 from "../../assets/audio_files/piano/E2.webm";
+import E3 from "../../assets/audio_files/piano/E3.webm";
+import E4 from "../../assets/audio_files/piano/E4.webm";
+import E5 from "../../assets/audio_files/piano/E5.webm";
+import Eb1 from "../../assets/audio_files/piano/Eb1.webm";
+import Eb2 from "../../assets/audio_files/piano/Eb2.webm";
+import Eb3 from "../../assets/audio_files/piano/Eb3.webm";
+import Eb4 from "../../assets/audio_files/piano/Eb4.webm";
+import Eb5 from "../../assets/audio_files/piano/Eb5.webm";
+import F1 from "../../assets/audio_files/piano/F1.webm";
+import F2 from "../../assets/audio_files/piano/F2.webm";
+import F3 from "../../assets/audio_files/piano/F3.webm";
+import F4 from "../../assets/audio_files/piano/F4.webm";
+import F5 from "../../assets/audio_files/piano/F5.webm";
+import G1 from "../../assets/audio_files/piano/G1.webm";
+import G2 from "../../assets/audio_files/piano/G2.webm";
+import G3 from "../../assets/audio_files/piano/G3.webm";
+import G4 from "../../assets/audio_files/piano/G4.webm";
+import G5 from "../../assets/audio_files/piano/G5.webm";
+import Gb1 from "../../assets/audio_files/piano/Gb1.webm";
+import Gb2 from "../../assets/audio_files/piano/Gb2.webm";
+import Gb3 from "../../assets/audio_files/piano/Gb3.webm";
+import Gb4 from "../../assets/audio_files/piano/Gb4.webm";
+import Gb5 from "../../assets/audio_files/piano/Gb5.webm";
 
 export const PianoServiceConfiguration = {
   html5PoolSize: 30,
-}
+};
 
 export class PianoService {
   constructor() {
-    Howler.html5PoolSize = PianoServiceConfiguration.html5PoolSize
+    Howler.html5PoolSize = PianoServiceConfiguration.html5PoolSize;
   }
 
   preLoadAudio() {
@@ -16,18 +80,18 @@ export class PianoService {
         src: this.audiofiles[key],
         preload: true,
         pool: 10,
-      })
-      this.piano[key] = note
-      ;(window as any).piano = this.piano
+      });
+      this.piano[key] = note;
+      (window as any).piano = this.piano;
     }
   }
 
   public play(timedNote: { note: string; velocity: number; duration: number }) {
-    console.log('playing', timedNote)
-    const audio = this.piano[timedNote.note]
-    audio.volume(timedNote.velocity * 0.1)
-    audio.duration(timedNote.duration)
-    audio.play()
+    console.log("playing", timedNote);
+    const audio = this.piano[timedNote.note];
+    audio.volume(timedNote.velocity * 0.1);
+    audio.duration(timedNote.duration);
+    audio.play();
   }
 
   // play(timedNote: string) {
@@ -39,72 +103,72 @@ export class PianoService {
   //   audio.play()
   // }
 
-  piano: { [key: string]: Howl } = {}
+  piano: { [key: string]: Howl } = {};
 
   audiofiles: { [key: string]: string } = {
-    Bb1: '../../../assets/audio_files/piano/Bb1.webm',
-    B1: '../../../assets/audio_files/piano/B1.webm',
-    C1: '../../../assets/audio_files/piano/C1.webm',
-    Db1: '../../../assets/audio_files/piano/Db1.webm',
-    D1: '../../../assets/audio_files/piano/D1.webm',
-    Eb1: '../../../assets/audio_files/piano/Eb1.webm',
-    E1: '../../../assets/audio_files/piano/E1.webm',
-    F1: '../../../assets/audio_files/piano/F1.webm',
-    Gb1: '../../../assets/audio_files/piano/Gb1.webm',
-    G1: '../../../assets/audio_files/piano/G1.webm',
-    Ab1: '../../../assets/audio_files/piano/Ab1.webm',
-    A1: '../../../assets/audio_files/piano/A1.webm',
-    Bb2: '../../../assets/audio_files/piano/Bb2.webm',
-    B2: '../../../assets/audio_files/piano/B2.webm',
-    C2: '../../../assets/audio_files/piano/C2.webm',
-    Db2: '../../../assets/audio_files/piano/Db2.webm',
-    D2: '../../../assets/audio_files/piano/D2.webm',
-    Eb2: '../../../assets/audio_files/piano/Eb2.webm',
-    E2: '../../../assets/audio_files/piano/E2.webm',
-    F2: '../../../assets/audio_files/piano/F2.webm',
-    Gb2: '../../../assets/audio_files/piano/Gb2.webm',
-    G2: '../../../assets/audio_files/piano/G2.webm',
-    Ab2: '../../../assets/audio_files/piano/Ab2.webm',
-    A2: '../../../assets/audio_files/piano/A2.webm',
-    Bb3: '../../../assets/audio_files/piano/Bb3.webm',
-    B3: '../../../assets/audio_files/piano/B3.webm',
-    C3: '../../../assets/audio_files/piano/C3.webm',
-    Db3: '../../../assets/audio_files/piano/Db3.webm',
-    D3: '../../../assets/audio_files/piano/D3.webm',
-    Eb3: '../../../assets/audio_files/piano/Eb3.webm',
-    E3: '../../../assets/audio_files/piano/E3.webm',
-    F3: '../../../assets/audio_files/piano/F3.webm',
-    Gb3: '../../../assets/audio_files/piano/Gb3.webm',
-    G3: '../../../assets/audio_files/piano/G3.webm',
-    Ab3: '../../../assets/audio_files/piano/Ab3.webm',
-    A3: '../../../assets/audio_files/piano/A3.webm',
-    Bb4: '../../../assets/audio_files/piano/Bb4.webm',
-    B4: '../../../assets/audio_files/piano/B4.webm',
-    C4: '../../../assets/audio_files/piano/C4.webm',
-    Db4: '../../../assets/audio_files/piano/Db4.webm',
-    D4: '../../../assets/audio_files/piano/D4.webm',
-    Eb4: '../../../assets/audio_files/piano/Eb4.webm',
-    E4: '../../../assets/audio_files/piano/E4.webm',
-    F4: '../../../assets/audio_files/piano/F4.webm',
-    Gb4: '../../../assets/audio_files/piano/Gb4.webm',
-    G4: '../../../assets/audio_files/piano/G4.webm',
-    Ab4: '../../../assets/audio_files/piano/Ab4.webm',
-    A4: '../../../assets/audio_files/piano/A4.webm',
-    Bb5: '../../../assets/audio_files/piano/Bb5.webm',
-    B5: '../../../assets/audio_files/piano/B5.webm',
-    C5: '../../../assets/audio_files/piano/C5.webm',
-    Db5: '../../../assets/audio_files/piano/Db5.webm',
-    D5: '../../../assets/audio_files/piano/D5.webm',
-    Eb5: '../../../assets/audio_files/piano/Eb5.webm',
-    E5: '../../../assets/audio_files/piano/E5.webm',
-    F5: '../../../assets/audio_files/piano/F5.webm',
-    Gb5: '../../../assets/audio_files/piano/Gb5.webm',
-    G5: '../../../assets/audio_files/piano/G5.webm',
-    Ab5: '../../../assets/audio_files/piano/Ab5.webm',
-    A5: '../../../assets/audio_files/piano/A5.webm',
-    Bb6: '../../../assets/audio_files/piano/Bb6.webm',
-    B6: '../../../assets/audio_files/piano/B6.webm',
-    C6: '../../../assets/audio_files/piano/C6.webm',
-    Db6: '../../../assets/audio_files/piano/Db6.webm',
-  }
+    Bb1,
+    B1,
+    C1,
+    Db1,
+    D1,
+    Eb1,
+    E1,
+    F1,
+    Gb1,
+    G1,
+    Ab1,
+    A1,
+    Bb2,
+    B2,
+    C2,
+    Db2,
+    D2,
+    Eb2,
+    E2,
+    F2,
+    Gb2,
+    G2,
+    Ab2,
+    A2,
+    Bb3,
+    B3,
+    C3,
+    Db3,
+    D3,
+    Eb3,
+    E3,
+    F3,
+    Gb3,
+    G3,
+    Ab3,
+    A3,
+    Bb4,
+    B4,
+    C4,
+    Db4,
+    D4,
+    Eb4,
+    E4,
+    F4,
+    Gb4,
+    G4,
+    Ab4,
+    A4,
+    Bb5,
+    B5,
+    C5,
+    Db5,
+    D5,
+    Eb5,
+    E5,
+    F5,
+    Gb5,
+    G5,
+    Ab5,
+    A5,
+    Bb6,
+    B6,
+    C6,
+    Db6,
+  };
 }
